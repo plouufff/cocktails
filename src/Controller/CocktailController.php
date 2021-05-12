076ad91a70fbe4da0369aee63e19f92c56443422
Ju <julien.d.duchesne@gmail.com>
2021-05-12 22:26:58 +0200

    remove unused Twig\Environment in CocktailController

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -9,20 +9,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class CocktailController extends AbstractController
 {
-    private Environment $twig;
     private EntityManagerInterface $entityManager;
     private CocktailRepository $cocktails;
 
     public function __construct(
-        Environment $twig,
         EntityManagerInterface $entityManager,
         CocktailRepository $cocktails
     ) {
-        $this->twig = $twig;
         $this->entityManager = $entityManager;
         $this->cocktails = $cocktails;
     }
