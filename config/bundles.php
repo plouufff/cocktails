01b0013375aa71cd09ae1aa2f65df0f8064d437a
Ju <julien.d.duchesne@gmail.com>
2022-05-13 00:02:44 +0200

    fix markdown (req twig/markdown-extra twig/extra-bundle and league/commonmark)

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
