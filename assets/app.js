49636234a3b8f80c3a45590fa1e4515bde591a06
ju <julien.d.duchesne@gmail.com>
2025-12-15 23:40:01 +0100

    fix(install): fix webpack config, assets and update npm dependencies

--- assets/app.js
+++ assets/app.js
@@ -6,31 +6,17 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-const $ = require('jquery');
+// const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-require('bootstrap');
+require("bootstrap");
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-  $('[data-toggle="popover"]').popover();
-
-  const jumbotronHeader = $("header .jumbotron");
-  jumbotronHeader.each(function() {
-    setBackgroundImageFromDataAttr($(this))
-  });
-});
-
-function setBackgroundImageFromDataAttr(element) {
-  if (element.attr('data-background')){
-    element.css('background-image', 'url(' + element.data('background') + ')');
-  }
-}
