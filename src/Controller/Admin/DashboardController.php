bbfa77e1fd2499672a2509d4371b3fc56693ae9f
ju <julien.d.duchesne@gmail.com>
2026-01-09 23:48:34 +0100

    feat(admin): use easyadmin pretty URLs

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,16 +7,15 @@ namespace App\Controller\Admin;
 use App\Entity\Cocktail;
 use App\Entity\Ingredient;
 use App\Entity\IngredientCategory;
-// use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
