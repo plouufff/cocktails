dddde40290a9197071946aca8cdea00903dce110
Julien Duchesne <julien.d.duchesne@gmail.com>
2026-03-27 21:57:25 +0100

    feat(): rework cocktail search with DTO (#52)

--- src/Controller/Api/CocktailController.php
+++ src/Controller/Api/CocktailController.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Dto\CocktailSearchDto;
 use App\Entity\Cocktail;
 use App\Repository\CocktailRepository;
 use App\Serializer\CocktailApiNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/cocktails', name: 'api_cocktails_', methods: ['GET'])]
@@ -21,18 +23,18 @@ class CocktailController extends AbstractController
     }
 
     #[Route(name: 'list')]
-    public function getAllCocktails(): JsonResponse
+    public function list(#[MapQueryString] CocktailSearchDto $cocktailSearchDto): JsonResponse
     {
         $normalizedData = array_map(
             fn ($cocktail): array => $this->normalizer->normalize($cocktail),
-            $this->cocktails->findAll()
+            $this->cocktails->search($cocktailSearchDto)
         );
 
         return $this->json($normalizedData);
     }
 
     #[Route('/{slug:cocktail}', name: 'get')]
-    public function getCocktail(Cocktail $cocktail): JsonResponse
+    public function get(Cocktail $cocktail): JsonResponse
     {
         $normalizedData = $this->normalizer->normalize($cocktail, 'json', ['groups' => ['cocktail:details']]);
 
